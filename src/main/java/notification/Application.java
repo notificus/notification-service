7e06fee0362ff46c939947270c89b7c622f8fbf6
Philippe Spino <Philippe.spino@gmail.com>
2018-10-03 11:30:10 -0400

    remove unecessary files

--- src/main/java/notification/Application.java
+++ src/main/java/notification/Application.java
@@ -1,7 +1,6 @@
-package Notification;
+package notification;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class Application {
     public static void main(String args[]) {
