2b542a5a9bbdfe4094b3ed55f108b1ec941f8dc1
Philippe Spino <Philippe.spino@gmail.com>
2018-10-03 11:16:32 -0400

    first commit

---
+++ src/main/java/Notification/Application.java
@@ -0,0 +1,10 @@
+package Notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Application {
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+}
